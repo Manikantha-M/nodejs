c3735f65a382f0049233b72947aaee93bb0e7527
Manikantha M <manikantha.m@neutrinos.co>
2023-12-19 09:05:10 +0530

    changes

--- node/app3.js
+++ node/app3.js
@@ -1,12 +1,12 @@
 /*
 Modules
-Node uses CommonJS, CommonJS modules are loaded synchronously (blocks the execution until loaded), while ES modules are loaded asynchronously.
+Node uses CommonJS (in old versions of node), CommonJS modules are loaded synchronously (blocks the execution until loaded), while ES modules are loaded asynchronously.
 
 Node JS module has exports object where we can store variables and functions.
 */
 
 const names = require('./names.js');
-const utils = require('./utils.js');
+const sayHi = require('./utils.js');
 console.log(names.john);
 console.log(names.peter);
-console.log(utils.sayHi('Mani'));
+console.log(sayHi('Mani'));
