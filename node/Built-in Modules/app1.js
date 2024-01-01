fc6872c8476f8449ea2bb5a72aa5b6709a89bbf5
Manikantha M <manikantha.m@neutrinos.co>
2024-01-01 07:58:54 +0530

    fs improvements

--- node/Built-in Modules/app1.js
+++ node/Built-in Modules/app1.js
@@ -16,10 +16,45 @@ console.log(
 
 const path = require('path');
 console.log(path.sep);
-const filePath = path.join('/folder','subfolder', 'file.txt');
+const filePath = path.join('/folder', 'subfolder', 'file.txt');
 console.log(filePath);
 const base = path.basename(filePath);
 console.log(base);
 
 const absolute = path.resolve(__dirname, 'folder', 'subfolder', 'file.txt');
 console.log(absolute);
+
+
+/* FS */
+
+// synchronous
+const { readFileSync, writeFileSync } = require('fs');
+
+const first = readFileSync('../files/first.txt', 'utf-8');
+console.log(first);
+
+writeFileSync('../files/new.txt', 'written using node js');
+
+writeFileSync('../files/new.txt', ' & this line is appended using flag a', { flag: 'a' });
+
+const readNewFile = readFileSync('../files/new.txt', 'utf-8');
+console.log(readNewFile);
+
+// asynchronous
+
+const { readFile, writeFile } = require('fs');
+
+readFile('../files/first.txt', 'utf-8', (err, result) => {
+    if (err) console.log(err);
+    else console.log(result);
+})
+
+writeFile('../files/new-async.txt', 'New file created using fs async', (err) => {
+    if (err) console.log(err);
+});
+
+
+readFile('../files/new-async.txt', 'utf-8', (err, result) => {
+    if (err) console.log(err);
+    else console.log(result);
+})
