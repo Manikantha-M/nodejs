929e4eb9dda96032a29748a0803cef6ee6ccaa50
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-01-05 09:36:04 +0530

    fundamentals of http request response cylce

---
+++ node/app8.js
@@ -0,0 +1,13 @@
+/*
+HTTP Request Response cycle
+
+Request:
+URL, Method, Headers, Body
+Request header:
+A request header is an HTTP header that can be used in an HTTP request to provide information about the request context, so that the server can tailor the response. For example, the Accept-* headers indicate the allowed and preferred formats of the response. Other headers can be used to supply authentication credentials (e.g. Authorization), to control caching, or to get information about the user agent or referrer, etc.
+
+Response:
+Status text, Status code, Headers, Body
+Response header:
+
+*/
