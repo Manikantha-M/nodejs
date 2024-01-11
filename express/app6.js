c864614e6d4ac484d62ad56870060ebc3641a136
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-01-11 09:11:02 +0530

    path parameters and query parameterd

--- express/app6.js
+++ express/app6.js
@@ -1,4 +1,33 @@
-// JSON Basics and Path Params
+/*
+JSON Basics and Path Parameters and Query Parameters
+
+Path Parameters:
+
+Usage: Path parameters are part of the URL path itself.
+Syntax: They are specified in the URL path by placing a placeholder in the path, often denoted by a colon (:) followed by the parameter name.
+Example: /users/:userId
+Usage Case: Path parameters are typically used to identify a specific resource or entity in the URL. For example, in the URL /users/123, 123 is a path parameter representing the userId.
+
+app.get('/users/:userId', (req, res) => {
+  const userId = req.params.userId;
+  // Handle the request based on the userId
+});
+
+
+Query Parameters:
+
+Usage: Query parameters are appended to the end of the URL after a question mark (?), separated by ampersands (&).
+Syntax: They are specified as key-value pairs, such as ?name=value.
+Example: /search?query=node&category=programming
+Usage Case: Query parameters are often used for filtering, sorting, or providing additional options to the server. They don't directly identify a resource like path parameters.
+
+app.get('/search', (req, res) => {
+  const query = req.query.query;
+  const category = req.query.category;
+  // Handle the search based on the query and category
+});
+
+*/
 
 const express = require('express');
 const {products} = require('./data');
@@ -17,6 +46,11 @@ app.get('/api/products/:id', (req, res)=> {
     const singleProduct = products.find(prod => req.params.id == prod.id.toString());
     if(!singleProduct) res.status(404).send('Product not found')
     else res.json(singleProduct)
+})
+
+app.get('/api/products/:id/reviews/:rid', (req, res) => {
+    console.log(req.params);
+    res.send('Hello world')
 })
 
 app.listen(5000, () => {
