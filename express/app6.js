61d18e8690ab34626615bf390f0a1b0e54fe9f8f
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-01-11 08:12:36 +0530

    path params

--- express/app6.js
+++ express/app6.js
@@ -1,7 +1,23 @@
-// JSON Basics
+// JSON Basics and Path Params
 
 const express = require('express');
+const {products} = require('./data');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Home Page</h1><a href="/api/products">products</a>`);
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map(({id, name, image}) => ({id, name, image}));
+    res.json(newProducts)
+})
+
+app.get('/api/products/:id', (req, res)=> {
+    const singleProduct = products.find(prod => req.params.id == prod.id.toString());
+    if(!singleProduct) res.status(404).send('Product not found')
+    else res.json(singleProduct)
+})
 
 app.listen(5000, () => {
     console.log('Server is listening on 5000');
