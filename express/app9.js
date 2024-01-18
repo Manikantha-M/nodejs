38071fd28dbd507586b528eec8207bdad89c5f66
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-01-18 07:44:52 +0530

    HTTP POST Method

--- express/app9.js
+++ express/app9.js
@@ -4,10 +4,26 @@ const path = require('path');
 const app = express();
 let { people } = require('./data');
 
-app.use(express.static(path.resolve(__dirname, './staticfiles/methods-public')));
+app.use(express.static(path.resolve(__dirname, './staticfiles/http-methods-public')));
+
+//parse form data
+app.use(express.urlencoded({extended:false}));
+// parse json
+app.use(express.json());
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({success: true, data: people})
+});
+
+app.post('/login', (req, res)=>{
+    const {name} = req.body;
+    if(name)return res.status(200).send(`Welcome ${name}`);
+    res.status(401).send('Please Proved credential');
+})
+
+app.post('/api/people', (req, res)=>{
+    console.log(req.body)
+    res.status(201).json({success:true, person:req.body.name});
 })
 
 app.listen(5000)
