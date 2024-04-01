7232e5b5f79d981c8f85dfb07ef35d3cb34773fd
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-01 06:24:02 +0530

    cleanup

--- express/app10.js
+++ express/app10.js
@@ -7,10 +7,4 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use('/api/people', peopleRouter)
 
-app.post('/login', (req, res)=>{
-    const {name} = req.body;
-    if(name)return res.status(200).send(`Welcome ${name}`);
-    res.status(401).send('Please Provide credentials');
-})
-
 app.listen(5000)
