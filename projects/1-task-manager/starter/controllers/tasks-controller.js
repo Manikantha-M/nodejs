cb98f6cd98ba42fd7c5de71322d53f7db0bb8183
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-05 11:42:02 +0530

    set up custom error class

--- projects/1-task-manager/starter/controllers/tasks-controller.js
+++ projects/1-task-manager/starter/controllers/tasks-controller.js
@@ -1,5 +1,6 @@
 const TaskModel = require('../models/task-model');
 const asyncWrapper = require('../middleware/async-wrapper');
+const {createCustomError} = require('../errors/custom-error');
 
 // const getAllTasks = async(req, res) => {
 //     try {
@@ -21,31 +22,33 @@ const createTask = asyncWrapper(async(req, res) => {
     res.status(201).json(task); 
 })
 
-const getTask =  asyncWrapper(async(req, res) => {
+const getTask =  asyncWrapper(async(req, res, next) => {
     const id = req.params.id;
     const task = await TaskModel.findOne({_id:id});
-    if(!task) return res.status(404).json({msg:'task not found'});
+    if(!task) {
+        return next(createCustomError('task not found', 404));
+    }
     res.status(200).json({status:true, task});
 })
 
-const updateTask =  asyncWrapper(async(req, res) => {
+const updateTask =  asyncWrapper(async(req, res, next) => {
     const {params:{id}, body} = req;
     const task = await TaskModel.findOneAndUpdate({_id:id}, body, {new:true, runValidators:true});
-    if(!task) return res.status(404).json({msg:'Could not update'});
+    if(!task)  return next(createCustomError('Could not update', 404));
     res.status(200).json({status:true, task});  
 })
 
-const editTask =  asyncWrapper(async(req, res) => {
+const editTask =  asyncWrapper(async(req, res, next) => {
     const {params:{id}, body} = req;
     const task = await TaskModel.findOneAndReplace({_id:id}, body, {new:true, runValidators:true});
-    if(!task) return res.status(404).json({msg:'Could not PUT'});
+    if(!task)  return next(createCustomError('Could not PUT', 404));
     res.status(200).json(task);
 });
 
-const deleteTask =  asyncWrapper(async(req, res) => {
+const deleteTask =  asyncWrapper(async(req, res, next) => {
     const id = req.params.id;
     const task = await TaskModel.findOneAndDelete({_id:id});
-    if(!task) return res.status(404).json({msg:'Could not delete'});
+    if(!task)  return next(createCustomError('Could not delete', 404));
     res.status(200).json(task);
 });
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask, editTask }
--- projects/1-task-manager/starter/middleware/error-handler.js
+++ projects/1-task-manager/starter/middleware/error-handler.js
@@ -1,5 +1,9 @@
+const {CustomAPIError} = require('../errors/custom-error')
 const errorHandler = (err, req, res, next) => {
-    return res.status(500).json(err);
+    if(err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({message: err.message});
+    }
+    return res.status(500).json({msg: 'Somethig went wrong'});
 }
 
 module.exports = errorHandler;
