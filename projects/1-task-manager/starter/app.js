b96f53e75277c4a71742d91d61fd4d9baee902d5
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-05 10:20:11 +0530

    should allways place middle wares in proper order

--- projects/1-task-manager/starter/app.js
+++ projects/1-task-manager/starter/app.js
@@ -9,15 +9,15 @@ const errorHandler = require('./middleware/error-handler');
 // middleware
 app.use(express.json());
 app.use(express.static('./public'));
-app.use(notFound);
-app.use(errorHandler);
+
 // routes
 app.get('/hello', (req, res)=>{
     res.send('Task Manager App');
 })
 
 app.use('/api/v1/tasks', taskRouter)
-
+app.use(notFound);
+app.use(errorHandler);
 
 const port = 3000;
 const startServer = async() => {
