2649b073cf109ce0ca51e6aa02b0ea23bc090f1e
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-07 06:30:53 +0530

    sort feature for Store API

--- projects/04-store-api/starter/controllers/products.js
+++ projects/04-store-api/starter/controllers/products.js
@@ -1,13 +1,19 @@
 const Product = require('../models/product');
 
 const getAllProducts = async(req, res) => {
-    const {featured, company, name} = req.query;
+    const {featured, company, name, sort} = req.query;
     const queryObj = {};
     queryObj.featured = featured == 'true';
     if(company) queryObj.company = company;
     if(name) queryObj.name = {$regex: name, $options:'i'}
     console.log(queryObj)
-   const products = await Product.find(queryObj);
+   let result = Product.find(queryObj);
+   if(sort){
+    const sortStr = sort.split(',').join(' ');
+    result = result.sort(sortStr);
+   }
+   const products = await result;
+
    res.status(200).json({products, nbHits: products.length})
 }
 
