9383d70fc5cabbbce69816a6e974c5398b511ed3
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-08 06:37:12 +0530

    router, controller setup is completed for JWT basics project

---
+++ projects/05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,10 @@
+const login = async (req, res) => {
+    res.send('Fake Login/Register/Sgnup route');
+}
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Match.floor(Math.random() * 100);
+    res.status(200).json({msg:`Hello, John Doe`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`});
+}
+
+module.exports = {login, dashboard}
