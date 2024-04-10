485432d100d37be5efe9c99cd9dc67fd71ca83ba
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-10 07:56:26 +0530

    UserModel to create a user

--- projects/4-jobs-api/starter/controllers/auth.js
+++ projects/4-jobs-api/starter/controllers/auth.js
@@ -1,5 +1,14 @@
+const {StatusCodes} = require('http-status-codes');
+const UserModel = require('../models/User')
+const {BadRequestError} = require('../errors');
+
 const register = async (req, res) => {
-    res.send('register user')
+    // const {name, email, password} = req.body;
+    // if(!name || !email || !password){
+    //     throw new BadRequestError('Please provide name, email, and password')
+    // }
+    const user = await UserModel.create(req.body);
+    res.status(StatusCodes.CREATED).json(user)
 }
 const login = async (req, res) => {
     res.send('login user')
