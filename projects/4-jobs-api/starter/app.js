3f628ace1b439e81e3dba8ce13dc4c2c46feb027
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-10 07:15:41 +0530

    controller, router, db connection, middlewares,a nd error handlers

---
+++ projects/4-jobs-api/starter/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// Connect DB
+const {connectToDatabase} = require('./db/connect');
+
+app.use(express.json());
+// extra packages
+
+// routes
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const startServer = async()=>{
+    try {
+        const connection = await connectToDatabase(process.env.MONGO_URI);
+        if(connection){
+          console.log('connected to the database');
+          app.listen(port, console.log(`server is listening on port ${port}`));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServer();
---
+++ projects/4-jobs-api/starter/db/connect.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = async(url) => {
+    try {
+       return await mongoose.connect(url);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
+}
+
+module.exports = {connectToDatabase};
