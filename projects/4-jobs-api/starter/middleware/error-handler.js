de010806d85ee98d1c2492e74b4008f5472faa86
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-11 14:50:43 +0530

    error messages set up

--- projects/4-jobs-api/starter/middleware/error-handler.js
+++ projects/4-jobs-api/starter/middleware/error-handler.js
@@ -6,12 +6,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg:err.message || 'Something went wrong, try again later'
   }
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
   if(err.code && err.code == 11000){
     customError.msg = `Duplicate User Registration: ${err.keyValue.email}`;
-    customError.statusCode = 400;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  if(err.name == 'CastError'){
+    customError.msg = `No item found with id: ${err.value}`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
   }
   return res.status(customError.statusCode).json({ msg: customError.msg });
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
