18042b67111d37aee5c587360b6afd48cbd51f12
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-11 15:26:13 +0530

    use security middlewares: helmet, xss, express rate limiter, and cors

--- projects/4-jobs-api/starter/app.js
+++ projects/4-jobs-api/starter/app.js
@@ -5,6 +5,11 @@ const app = express();
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 const authenticateUser = require('./middleware/authentication');
+// Extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -12,9 +17,18 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // Connect DB
 const {connectToDatabase} = require('./db/connect');
-
+app.set('trust proxy', 1);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+}));
 
 // routes
 app.use('/api/v1/auth', authRouter);
