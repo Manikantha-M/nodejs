65b618f9b499b6b0a6fe2a8b0be645327a062c10
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-06-10 06:11:52 +0530

    make test user read only for job apis

--- projects/5-jobster-api/starter/routes/jobs.js
+++ projects/5-jobster-api/starter/routes/jobs.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const checkTestUser = require('../middleware/testuser');
 
 const {getAllJobs, getJob, createJob, updateJob, deleteJob } = require('../controllers/jobs');
 
-router.route('/').post(createJob).get(getAllJobs);
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
+router.route('/').post(checkTestUser, createJob).get(getAllJobs);
+router.route('/:id').get(getJob).delete(checkTestUser, deleteJob).patch(checkTestUser, updateJob)
 
 module.exports = router;
