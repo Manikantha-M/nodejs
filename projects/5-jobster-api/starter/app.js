a67bc84b638c93885a15bf790cec09eba8928823
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-06-10 07:05:22 +0530

    api rate limiter set up for login and register routes

--- projects/5-jobster-api/starter/app.js
+++ projects/5-jobster-api/starter/app.js
@@ -29,13 +29,15 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
-}));
+
+// Rate limiter for entire application:
+// app.use(rateLimit({
+// 	windowMs: 15 * 60 * 1000, // 15 minutes
+// 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+// 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+// 	// store: ... , // Redis, Memcached, etc. See below.
+// }));
 
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
--- projects/5-jobster-api/starter/routes/auth.js
+++ projects/5-jobster-api/starter/routes/auth.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 const authenticateUser = require('../middleware/authentication');
 const checkTestUser = require('../middleware/testuser');
+const rateLimit = require('express-rate-limit');
 
 const {login, register, updateUser} = require('../controllers/auth');
+const apiRateLimiter = rateLimit({
+	windowMs: 5 * 60 * 1000, // 5 minutes
+	limit: 3, // Limit each IP to 3 requests per `window` (here, per 5 minutes).
+    messsage:{msg:'Too many requests from this IP, please try again after 5 mins'},
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+});
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', apiRateLimiter, register);
+router.post('/login', apiRateLimiter, login);
 router.patch('/updateUser', authenticateUser, checkTestUser, updateUser)
 module.exports = router;
