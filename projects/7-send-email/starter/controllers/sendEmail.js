32ff5d165eb65fe75eea5cc2b0ef883634f8060a
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-06-16 16:54:58 +0530

    send email api is completed

--- projects/7-send-email/starter/controllers/sendEmail.js
+++ projects/7-send-email/starter/controllers/sendEmail.js
@@ -1,5 +1,22 @@
+const nodemailer = require('nodemailer');
+
 const sendEmail = async (req, res)=> {
-    res.send('send email func');
-}
+    const testAccount = await nodemailer.createTestAccount();
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+    let info = await transporter.sendMail({
+        from: 'manikanta.iiitnuz@gmail.com',
+        to: 'mani@gmail.com',
+        subject: 'Hello',
+        html: '<h2>Sending Email with Nodejs</h2>'
+    })
+    res.send(info);
+};
 
 module.exports = sendEmail;
