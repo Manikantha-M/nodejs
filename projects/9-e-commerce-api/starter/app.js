53e53e29b1f746d737aa42168d4166bbdc8b2ea6
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-06-18 07:56:21 +0530

    authentication router, and auth controller  setup

--- projects/9-e-commerce-api/starter/app.js
+++ projects/9-e-commerce-api/starter/app.js
@@ -1,22 +1,28 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 // Connect DB
 const { connectToDatabase } = require('./db/connect');
+// Auth Router
+const authRouter = require('./routes/authRoutes');
+
 // middlewares
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(morgan('tiny'));
 app.use(express.json());
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
 app.get('/', (req, res)=> {
     res.send('E-Commerce API')
 })
+app.use('/api/v1/auth',authRouter)
 
-
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
