1a83ace502597f0c7545b007c841189d33ca893f
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-06-18 07:13:54 +0530

    packages and middleware import to app js

--- projects/9-e-commerce-api/starter/app.js
+++ projects/9-e-commerce-api/starter/app.js
@@ -1,17 +1,34 @@
-console.log('E-Commerce API');
-const port = process.env.PORT || 3000;
-// Connect DB
-// const {connectToDatabase} = require('./db/connect');
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
 
-// const startServer = async()=>{
-//     try {
-//         const connection = await connectToDatabase(process.env.MONGO_URI);
-//         if(connection){
-//           console.log('connected to the database');
-//           app.listen(port, console.log(`server is listening on port ${port}`));
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// startServer();
+const app = express();
+// Connect DB
+const { connectToDatabase } = require('./db/connect');
+// middlewares
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+app.use(express.json());
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+app.get('/', (req, res)=> {
+    res.send('E-Commerce API')
+})
+
+
+
+const port = process.env.PORT || 5000;
+
+const startServer = async () => {
+    try {
+        const connection = await connectToDatabase(process.env.MONGO_URI);
+        if (connection) {
+            console.log('connected to the database');
+            app.listen(port, console.log(`server is listening on port ${port}`));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServer();
