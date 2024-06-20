c0a0786a016a399da538348772b3a3805e682b5a
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-06-20 08:17:15 +0530

    utils folder for useful modules and index js for single export for easy access

--- projects/9-e-commerce-api/starter/controllers/authController.js
+++ projects/9-e-commerce-api/starter/controllers/authController.js
@@ -1,15 +1,24 @@
 const UserSchema = require('../model/user-model');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const {createJWT} = require('../utils');
+
 
 const register = async (req, res) => {
-    const { email } = req.body;
+    const { email, name, password } = req.body;
     const emailAlreadyExists = await UserSchema.findOne({email});
     if(emailAlreadyExists) {
         throw new CustomError.BadRequestError('Email already exists');
     }
-    const user = await UserSchema.create(req.body);
-    res.status(StatusCodes.CREATED).json({ user })
+    // First registered user is an Admin
+    const isFirstAccount = (await UserSchema.countDocuments({})) == 0;
+    const role = isFirstAccount ? 'admin' : 'user'
+    const user = await UserSchema.create({ email, name, password, role });
+    
+    const tokenUser = {name:user.name, userId:user._id, role:user.role}; 
+    const token = createJWT({payload:tokenUser})
+    
+    res.status(StatusCodes.CREATED).json({ user:tokenUser, token });
 };
 const login = async (req, res) => {
     res.send('login user')
