5f2a31dc5d1f7251d5f3c8493ef678962ae0f915
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-06-27 07:28:37 +0300

    authenticate user middleware and authorize permissions middleware

--- projects/9-e-commerce-api/starter/middleware/authentication.js
+++ projects/9-e-commerce-api/starter/middleware/authentication.js
@@ -1,15 +1,26 @@
-const CustomErr = require('../errors');
+const CustomError = require('../errors');
 const {isTokenValid} = require('../utils');
 
 const authenticateUser = async(req, res, next) => {
     const token = req.signedCookies.token;
     if(!token){
-        console.log('error, no token present');
+        throw new CustomError.UnauthenticatedError('Authentication invalid');
     }
-    else{
-    console.log('token present');
-    }   
+
+    try {
+        const {name, userId, role} = isTokenValid({token});
+        req.user = {name, userId, role};
+        next();
+    } catch (error) {
+        
+    }
+}
+
+const authorizePermissions = (req, res, next) => {
+    if(req.user.role != 'admin'){
+        throw new CustomError.UnauthorizedError('Unauthorized to access this route')
+    }
     next();
 }
 
-module.exports = {authenticateUser}
+module.exports = {authenticateUser, authorizePermissions}
