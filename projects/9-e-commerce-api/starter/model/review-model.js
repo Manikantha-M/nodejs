24fa0283e52610a02c07012bf122451ee00f109f
Manikantha-M <manikanta.iiitnuz@gmail.com>
2026-01-05 22:25:03 +0300

    review model aggregator completed

--- projects/9-e-commerce-api/starter/model/review-model.js
+++ projects/9-e-commerce-api/starter/model/review-model.js
@@ -29,5 +29,36 @@ const ReviewSchema = mongoose.Schema({
         required:true
     }
 }, {timestamps:true});
-ReviewSchema.index({product:1, user:1}, {unique:true})
+ReviewSchema.index({product:1, user:1}, {unique:true});
+
+ReviewSchema.statics.calculateAvgRating = async function (productId) {
+    const result = await this.aggregate([
+        {$match:{product:productId}},
+        {$group:{
+            _id:null,
+            averageRating:{$avg:'$rating'},
+            numOfReviews:{$sum:1}
+        }}
+    ]);
+    try {
+        await this.model('Product').findOneAndUpdate({_id:productId}, {
+            averageRating: Math.ceil(result[0]?.averageRating || 0),
+            numOfReviews: result[0]?.numOfReviews || 0
+        })
+    } catch (error) {
+        
+    }
+    console.log(result, 'aggregate result')
+}
+
+ReviewSchema.post('save', async function (){
+    await this.constructor.calculateAvgRating(this.product);
+    console.log('post save hook called');
+});
+
+ReviewSchema.post('deleteOne', { document: true, query: false }, async function (){
+    await this.constructor.calculateAvgRating(this.product);
+    console.log('post remove hook called');
+});
+
 module.exports = mongoose.model('Review', ReviewSchema);
