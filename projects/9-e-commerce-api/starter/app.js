4b92a51a337a83ee4a8d9fab58f22ae57f502fc8
Manikantha-M <manikanta.iiitnuz@gmail.com>
2026-01-10 09:38:04 +0300

    ecommerce index html

--- projects/9-e-commerce-api/starter/app.js
+++ projects/9-e-commerce-api/starter/app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express');
+const cors = require('cors');
 
 const app = express();
 // Connect DB
@@ -17,8 +22,17 @@ const orderRouter = require('./routes/orderRoutes');
 // middlewares
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+    windowMS:15*60*1000,
+    max:60,
+}));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static('./public'));
